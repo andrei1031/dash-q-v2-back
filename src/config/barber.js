e82e09da54ad9ef0d57a26ead6dce16834eee169
Reuben Andrei Saldivar <andreisantossaldivar@gmail.com>
2026-02-01 12:17:59 +0800

    Log in code changed

--- src/config/barber.js
+++ src/config/barber.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-const BARBER_SIGNUP_CODE = process.env.BARBER_SIGNUP_CODE || 'DEFAULT_SIGNUP_CODE';
-const BARBER_LOGIN_PIN = process.env.BARBER_LOGIN_PIN || "1234";
+const BARBER_SIGNUP_CODE = process.env.BARBER_SIGNUP_CODE || '080825';
+const BARBER_LOGIN_PIN = process.env.BARBER_LOGIN_PIN || "080825";
 
 if (!BARBER_SIGNUP_CODE || !BARBER_LOGIN_PIN) {
   throw new Error(
