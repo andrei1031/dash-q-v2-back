d350fc46f1e51327e3a53e2d213ad79a4e4948ac
Reuben Andrei Saldivar <andreisantossaldivar@gmail.com>
2026-04-01 01:29:05 +0800

    ABORT

--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const { 
-    
     slots, 
     book, 
     reject, 
@@ -11,15 +10,20 @@ const {
     get_all_appointments,
     get_barber_appointments,
     process_appointments
-
 } = require('../controller/appointmentController');
+
+// All routes here are automatically prefixed with /api/appointments by server.js
 
 router.get('/slots', slots);
 router.post('/book', book);
 router.put('/reject', reject);
 router.put('/approve', approve);
 router.get('/my/:userId', get_customer_appointments);
-router.get('/appointments/barber/:barberId', get_barber_appointments);
-router.get('/test/process-appointments', process_appointments);
+
+// FIX 1: Change '/appointments/barber/:barberId' to '/barber/:barberId'
+router.get('/barber/:barberId', get_barber_appointments);
+
+// FIX 2: Change '/test/process-appointments' to '/process' (to match what your frontend likely expects)
+router.get('/process', process_appointments); 
 
 module.exports = router;
