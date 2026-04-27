b0cd970835f38c6740d6a6b7933020a377acc959
Reuben Andrei Saldivar <andreisantossaldivar@gmail.com>
2026-04-27 23:09:12 +0800

    Report Feature FIx

--- src/routes/reportsRoutes.js
+++ src/routes/reportsRoutes.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { 
-    
     submit_reports,
     get_all_reports,
     admin_reports_resolve,
     get_user_submitted_reports
-
 } = require('../controller/reportsController');
 
-router.post('/reports', submit_reports);
-router.get('/reports/my/:userId', get_user_submitted_reports);
+// Change '/reports' to '/'
+router.post('/', submit_reports); 
+
+// Change '/reports/my/:userId' to '/my/:userId'
+router.get('/my/:userId', get_user_submitted_reports); 
 
 module.exports = router;
