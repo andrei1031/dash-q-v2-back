4b0b9c63b6d88c3d7bd7c58d61cc727c6c8bdfb9
Reuben Andrei Saldivar <andreisantossaldivar@gmail.com>
2026-04-28 00:08:23 +0800

    Report Feature FIx

--- src/routes/reportsRoutes.js
+++ src/routes/reportsRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { unban_user, submit_reports, get_user_submitted_reports } = require('../controller/reportsController');
 
 const { 
     submit_reports,
     get_all_reports,
     admin_reports_resolve,
-    get_user_submitted_reports
+    get_user_submitted_reports,
+    unban_user
 } = require('../controller/reportsController');
 
-// Change '/reports' to '/'
-router.put('/unban/:userId', unban_user); 
+// Define your routes...
 router.post('/', submit_reports);
+router.put('/unban/:userId', unban_user);
 router.get('/my/:userId', get_user_submitted_reports);
 
 module.exports = router;
